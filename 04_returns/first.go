2986ed4d0f0b79686196279b4a8a599514e8a13b
Nicolas Marsal <nicolas.marsal@gmail.com>
2016-06-13 10:06:06 +0000

    04

--- 04_returns/first.go
+++ 04_returns/first.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 // TODO: Make a function that adds two numbers and returns the result
+func add (a int, b int) {
+    return a+b
+}
 
 func main() {
     fmt.Println(add(4, 5))
