f478785d341fc2c1e233f7613bd7b20018867066
Nicolas Marsal <nicolas.marsal@gmail.com>
2016-06-13 10:09:09 +0000

    05

--- 05_structs/first.go
+++ 05_structs/first.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 // TODO: Define a Computer struct and it's properties
-
+type Computer struct {
+    Brand string
+    Model string
+    Price int
+}
 func main() {
     computer := Computer{
         Brand: "Apple",
